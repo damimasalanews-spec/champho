babce6d216c2ae8c19166d0a598742239096d108
damimasalanews-spec <damimasalanews@gmail.com>
2026-09-21 01:09:39 +0545

    Use hardened migration runner at startup

--- server/main.ts
+++ server/main.ts
@@ -1,26 +1,8 @@
-import { readFile } from "node:fs/promises";
-import { join } from "node:path";
 import { pool } from "./db.js";
 import { start, type ServerApp } from "./index.js";
-
-const migrations = [
-  "db/001_game_rooms_and_submissions.sql",
-  "db/002_protocol_constraints.sql",
-  "db/003_protocol_indexes_and_immutability.sql",
-  "db/004_connection_version.sql"
-];
+import { runMigrations } from "./migrations.js";
 
 let app: ServerApp | undefined;
-
-async function runMigrations(): Promise<void> {
-  for (const migration of migrations) {
-    console.log(`[startup] Applying migration ${migration}`);
-    const sql = await readFile(join(process.cwd(), migration), "utf8");
-    await pool.query(sql);
-  }
-
-  console.log(`[startup] PostgreSQL migrations applied (${migrations.length})`);
-}
 
 async function shutdown(signal: string): Promise<void> {
   console.log(`[shutdown] Received ${signal}; closing server`);
@@ -41,7 +23,7 @@ process.once("SIGTERM", () => {
   void shutdown("SIGTERM").finally(() => process.exit(0));
 });
 
-void runMigrations()
+void runMigrations(pool)
   .then(() => start())
   .then((started) => {
     app = started;
